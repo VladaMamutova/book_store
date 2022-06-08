9b7a66e7c418b4e79ea99ce352a92463139de276
Ivan Konovalov <thejadeonwot@gmail.com>
2022-06-08 18:39:24 +0300

    Обучение по фронтенду 8.06.2022

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,23 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  def to_s(full: false)
+    full ? self.full : self.short
+  end
+
+  def short
+    if self.middle_name.present?
+      self.last_name + ' ' + self.first_name[0] + '.' + self.middle_name[0] + '.'
+    else
+      self.last_name + ' ' + self.first_name[0] + '.'
+    end
+  end
+
+  def full
+    if self.middle_name.present?
+      self.last_name + ' ' + self.first_name + ' ' + self.middle_name
+    else
+      self.last_name + ' ' + self.first_name
+    end
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,5 @@
 class Genre < ApplicationRecord
+  def to_s
+    self.name
+  end
 end
