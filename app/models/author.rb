fd041ca0afcb437d3e97ea733e261364262fda9e
Мамутова Влада Александровна <vmamutova@phoenixit.ru>
2022-06-09 13:02:44 +0300

    Практика 5: Упрощение шаблона

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,15 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  def to_s(full: false)
+    full ? full_name : "#{last_name} #{initials}"
+  end
+
+  def full_name
+    "#{first_name} #{middle_name} #{last_name}"
+  end
+
+  def initials
+    "#{first_name[0]}." + (middle_name.present? ? " #{middle_name[0]}." : '')
+  end
 end
