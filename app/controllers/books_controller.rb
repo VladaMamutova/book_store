a2dfddf1a90e2b277a9e8a52600c163b24d6d374
Мамутова Влада Александровна <vmamutova@phoenixit.ru>
2022-07-14 22:47:26 +0300

    Практика 12: Использование `form_for` для создания формы

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :author_id, :genre_id)
   end
 end
